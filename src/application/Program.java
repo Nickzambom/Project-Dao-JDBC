6c760749506c10c7e8466aae2a47364fe5d31899
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-09-15 11:49:05 -0300

    Criação da Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Malucos");
+		Seller obj1 = new Seller(1, "Maicon Lucas", "nicholasnk@gmail.com", new Date(), 500.00, obj);
+
 		System.out.println(obj);
+		System.out.println(obj1);
 	}
 
 }
