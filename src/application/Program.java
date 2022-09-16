3af550e2beb9c7be6ce9e1728245ee09f985eaee
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-09-16 10:51:20 -0300

    FindAll implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,18 @@ public class Program {
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		System.out.println("=== TEST 1: seller findByDepartment ===");
+		System.out.println();
+		System.out.println("=== TEST 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.finByDepartment(department);
-		for(Seller obj : list) {
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		System.out.println();
+
+		System.out.println("=== TEST 3: seller findAll ===");
+		List<Seller> list2 = sellerDao.findAll();
+		for (Seller obj : list2) {
 			System.out.println(obj);
 		}
 	}
